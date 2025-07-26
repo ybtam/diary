f76fbb7df9b9b98240945b29cb505963b485fdaf
ybtam <tam.yi3d@gmail.com>
2025-07-26 13:02:41 +0200

    update user schema

--- apps/db/src/schemas/user/schema.ts
+++ apps/db/src/schemas/user/schema.ts
@@ -1,10 +1,15 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { jsonb, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
-  createdAt: timestamp('created_at').notNull().defaultNow(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
   email: text('email').notNull().unique(),
   firstName: text('first_name'),
   id: serial('id').primaryKey(),
   lastName: text('last_name'),
   password: text('password').notNull(),
+  settings: jsonb('settings'),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
 })
