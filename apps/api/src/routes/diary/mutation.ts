ebfc9cf3eca93b45b07588de37fc0af36924a790
Yi Tam <47603803+ybtam@users.noreply.github.com>
2025-07-27 12:45:09 +0200

    Update mutation.ts

--- apps/api/src/routes/diary/mutation.ts
+++ apps/api/src/routes/diary/mutation.ts
@@ -17,7 +17,7 @@ const update = protectedProcedure.input(updateDiaryEntrySchema).mutation(async (
   return db.update(diaryEntries).set(dataToUpdate).where(eq(diaryEntries.id, id))
 })
 
-const remove = protectedProcedure
+const delete = protectedProcedure
   .input(z.object({ id: z.number() }))
   .mutation(async ({ input }) => {
     return db.delete(diaryEntries).where(eq(diaryEntries.id, input.id))
@@ -25,6 +25,6 @@ const remove = protectedProcedure
 
 export default {
   create,
-  delete: remove,
+  delete,
   update,
 }
