1db2df8f978811b41e74a8171a8b62728a6a98c5
ybtam <tam.yi3d@gmail.com>
2025-08-03 20:37:31 +0200

    set server host to 0.0.0.0

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -8,7 +8,7 @@ const server = createServer()
 
 ;(async () => {
   try {
-    await server.listen({ port: 4000 })
+    await server.listen({ host: '0.0.0.0', port: 4000 })
     console.log('Server is running on http://localhost:4000')
   } catch (err) {
     server.log.error(err)
@@ -17,4 +17,3 @@ const server = createServer()
 })()
 
 export type { AppRouter, RouterInput, RouterOutput } from './server/router.ts'
-
